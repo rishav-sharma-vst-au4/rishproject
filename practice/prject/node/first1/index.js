35b046d065d50a21c4e426d16916bd3e6883ce90
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2019-11-01 16:19:18 +0530

    Create index.js

---
+++ practice/prject/node/first1/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const exphbs = require('express-handlebars');
+const multer = require('multer');
